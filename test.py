c3ebe68b5dd3a8802b29b858166ab11f5ba64634
Michael Romer <mvromer@gmail.com>
2019-12-05 17:18:15 -0600

    Adding temp test script

---
+++ test.py
@@ -0,0 +1,11 @@
+import torch
+from musicml import *
+
+h = Hyperparameters( vocab_size=5 )
+mt = MusicTransformer( h )
+S = 2
+T = 2
+inp = torch.randint( h.vocab_size, (S,) )
+out = torch.randint( h.vocab_size, (T,) )
+mask = create_attention_mask( T, S )
+z = mt( inp, out, mask )
